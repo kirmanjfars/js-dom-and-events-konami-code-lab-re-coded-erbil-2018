f4aa5f192be0808c7616125f4aa30549ff5f9470
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-08-22 18:16:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,6 +7,7 @@ function init() {
    
    document.body.addEventListener('keydown', function(e) {
      
-     const key = parseInt(e.detail  || e.which)
+     const key = parseInt(e.detail  || e.which); 
+     
    });
 }
